513d4d4dd0bdb17aaa6af79f5d1a27f95068dd82
Parker Zach <pkzach123@gmail.com>
2020-02-10 11:10:40 -0600

    Added classes
    Added User and Address classes

---
+++ src/User.java
@@ -0,0 +1,11 @@
+
+public class User {
+
+	private String personCode;
+	private Address address;
+	private String firstName;
+	private String lastName;
+	private String brokerStatus;
+	private String secIdentity;
+	
+}
