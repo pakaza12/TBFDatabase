e93029f48c794f030153232404d6817b2168b43c
Parker Zach <pkzach123@gmail.com>
2020-02-11 15:04:09 -0600

    Should get input for persons

--- src/com/tbf/DataConverter.java
+++ src/com/tbf/DataConverter.java
@@ -2,8 +2,10 @@ package com.tbf;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.Set;
 
 public class DataConverter {
 
@@ -18,12 +20,17 @@ public class DataConverter {
 			
 			for(int i = 0; i < size; i++) {
 				String token[] = input.nextLine().split(";", -5);
+				//ToDo, put if statement for broker if it breaks code
 				String broker[] = token[1].split(",", -2);
 				String names[] = token[2].split(",", -2);
 				String address[] = token[3].split(",");
-				Address p1 = Address(address[0], address[1], address[2], address[3], address[4]);
+				Address p1 = new Address(address[0], address[1], address[2], address[3], address[4]);
 				String email[] = token[4].split(",");
-				inputUsers[i] = User(token[0], );
+				Set<String> emails = new HashSet<String>();
+				for(int j = 0; j < email.length; j++) {
+					emails.add(email[j]);
+				}
+				inputUsers[i] = new User(token[0], p1, names[1], names[0], broker[0], broker[1], emails);
 			}
 			
 
