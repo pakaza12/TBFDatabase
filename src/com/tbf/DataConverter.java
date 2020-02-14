5e49d6b6d8945a6d73231df28be8c07e0119a5a6
Jayden <jayden.carlon13@gmail.com>
2020-02-14 14:03:40 -0600

    Update DataConverter.java

--- src/com/tbf/DataConverter.java
+++ src/com/tbf/DataConverter.java
@@ -1,3 +1,13 @@
+/*
+ * Authors: Parker Z and Jayden C.
+ * This program has two methods implemented in it. The first method takes in a file thats a report of user data. The method then 
+ * breaks up each part to put into an xml file.
+ * The second method does similar actions. It takes in a file containing assets to a specific user and splits it up and prints to an xml file
+ * Date:02/14/20
+ */
+
+
+
 package com.tbf;
 
 import java.io.File;
@@ -24,9 +34,9 @@ public class DataConverter {
 	 */
 	public static void Persons() {
 		try {
-
+			
 			Scanner input = new Scanner(new File("data/Persons.dat"));
-
+		
 			//Get the size of the file (how many lines of data there are an initialize an array of users
 			int size = Integer.parseInt(input.nextLine());
 			User inputUsers[] = new User[size];
