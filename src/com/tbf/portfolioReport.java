a6402026cfdfaedb7e1a4e54523efad89bc6e348
Jayden <jayden.carlon13@gmail.com>
2020-02-26 15:23:47 -0600

    Update portfolioReport.java

--- src/com/tbf/portfolioReport.java
+++ src/com/tbf/portfolioReport.java
@@ -1,10 +1,36 @@
 package com.tbf;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class portfolioReport {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	
+	
+	public static double value(HashMap<String, Double> a, Asset[] i) {
+	double value = 0;
+	
+	Set<String> b = a.keySet();
+	for(String c : b) {
+		for(int j = 0; j < i.length; j++) {
+			if(c.contains(i[j].getCode())) {
+				if(i[j].contains("Deposit")) {
+					
+				}
+			}
+		}
 	}
-
+	
+		
+	return value;
+	}
+	
+	
+	public static void summary (Portfolio[] report, User[] p, Asset[] a) {
+		for(Portfolio s: report) {
+			
+		System.out.println();
+		
+	}
+	}
 }
