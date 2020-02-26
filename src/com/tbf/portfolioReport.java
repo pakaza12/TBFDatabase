ea7c171fb105512bba6a17dd58aef56fb537df76
Jayden <jayden.carlon13@gmail.com>
2020-02-26 15:56:34 -0600

    value method

--- src/com/tbf/portfolioReport.java
+++ src/com/tbf/portfolioReport.java
@@ -6,16 +6,18 @@ import java.util.Set;
 public class portfolioReport {
 	
 	
-	
 	public static double value(HashMap<String, Double> a, Asset[] i) {
 	double value = 0;
 	
-	Set<String> b = a.keySet();
-	for(String c : b) {
+	for(HashMap.Entry<String, Double> c : a.entrySet()) {
 		for(int j = 0; j < i.length; j++) {
-			if(c.contains(i[j].getCode())) {
-				if(i[j].contains("Deposit")) {
-					
+			if(c.getKey().contains(i[j].getCode())) {
+				if(i[j]instanceof Deposit) {
+					value += c.getValue();
+				} else if(i[j]instanceof Stocks) {
+					value += (((Stocks)i[j]).getSharePrice() * c.getValue());
+				} else if(i[j]instanceof PrivateInvestment) {
+					value += (((PrivateInvestment)i[j]).getTotalValue() * c.getValue()/100);
 				}
 			}
 		}
@@ -24,6 +26,8 @@ public class portfolioReport {
 		
 	return value;
 	}
+	
+	
 	
 	
 	public static void summary (Portfolio[] report, User[] p, Asset[] a) {
