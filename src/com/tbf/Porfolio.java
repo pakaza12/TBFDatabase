88a4620664146be3cae5cadd01e600ea130fae20
Parker Zach <pkzach123@gmail.com>
2020-02-26 12:46:46 -0600

    Update Porfolio.java

--- src/com/tbf/Porfolio.java
+++ src/com/tbf/Porfolio.java
@@ -1,5 +1,8 @@
 package com.tbf;
 
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * This is a class that will connect the User class and the Asset class
  * 
@@ -8,4 +11,62 @@ package com.tbf;
  */
 public class Porfolio {
 
+	private String portfolioCode;
+	private String OwnerCode;
+	private String managerCode;
+	private String beneficiaryCode;
+	private List<HashMap<String, Double>> assetList;
+	
+	public Porfolio(String portfolioCode, String ownerCode, String managerCode, String beneficiaryCode,
+			List<HashMap<String, Double>> assetList) {
+		super();
+		this.portfolioCode = portfolioCode;
+		OwnerCode = ownerCode;
+		this.managerCode = managerCode;
+		this.beneficiaryCode = beneficiaryCode;
+		this.assetList = assetList;
+	}
+	
+	public String getPortfolioCode() {
+		return portfolioCode;
+	}
+	
+	public void setPortfolioCode(String portfolioCode) {
+		this.portfolioCode = portfolioCode;
+	}
+	
+	public String getOwnerCode() {
+		return OwnerCode;
+	}
+	
+	public void setOwnerCode(String ownerCode) {
+		OwnerCode = ownerCode;
+	}
+	
+	public String getManagerCode() {
+		return managerCode;
+	}
+	
+	public void setManagerCode(String managerCode) {
+		this.managerCode = managerCode;
+	}
+	
+	public String getBeneficiaryCode() {
+		return beneficiaryCode;
+	}
+	
+	public void setBeneficiaryCode(String beneficiaryCode) {
+		this.beneficiaryCode = beneficiaryCode;
+	}
+	
+	public List<HashMap<String, Double>> getAssetList() {
+		return assetList;
+	}
+	
+	public void setAssetList(List<HashMap<String, Double>> assetList) {
+		this.assetList = assetList;
+	}
+	
+	
+	
 }
