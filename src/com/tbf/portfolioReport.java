4f8ef7c19d0d41114baf4728453cebd578bba198
Parker Zach <pkzach123@gmail.com>
2020-02-26 20:59:45 -0600

    Update portfolioReport.java

--- src/com/tbf/portfolioReport.java
+++ src/com/tbf/portfolioReport.java
@@ -27,30 +27,46 @@ public class portfolioReport {
 
 	public static double getAggregateRisk(HashMap<String, Double> a, Asset[] i, double value) {
 		double risk = 0;
-		// ω=b+e(−125,500/v)= 0.32 +e(−125,500/1212500.00)= 1.22167
+
 		for (HashMap.Entry<String, Double> c : a.entrySet()) {
 			for (int j = 0; j < i.length; j++) {
-				if (i[j] instanceof PrivateInvestment) {
-					double constant = (-125500) / ((PrivateInvestment) c).getTotalValue();
-					double omega = ((PrivateInvestment) c).getBaseOmegaMeasure() + Math.exp(constant);
-					risk += omega * (c.getValue() / value);
-				} else if (i[j] instanceof Stocks) {
-					double beta = ((Stocks) c).getBaseRateOfReturn() * ((Stocks) c).getSharePrice() * c.getValue()
-							+ 4 * ((Stocks) c).getQuarterlyDividend() * c.getValue();
-					risk += beta * (c.getValue() / value);
+				if (c.getKey().contains(i[j].getCode())) {
+					if (i[j] instanceof PrivateInvestment) {
+						double constant = (-125500) / ((PrivateInvestment) c).getTotalValue();
+						double omega = ((PrivateInvestment) c).getBaseOmegaMeasure() + Math.exp(constant);
+						risk += omega * (c.getValue() / value);
+					} else if (i[j] instanceof Stocks) {
+						double beta = ((Stocks) c).getBaseRateOfReturn() * ((Stocks) c).getSharePrice() * c.getValue()
+								+ 4 * ((Stocks) c).getQuarterlyDividend() * c.getValue();
+						risk += beta * (c.getValue() / value);
+					}
 				}
 			}
 		}
 
 		return risk;
 	}
+	
+	public static double getAnnualReturns(HashMap<String, Double> a, Asset[] i) {
+		double annualReturn = 0;
+		
+		for(HashMap.Entry<String, Double> c : a.entrySet()) {
+			for(int j = 0; j < i.length; j++) {
+				if (c.getKey().contains(i[j].getCode())) {
+					
+				}
+			}
+		}
+		
+		return annualReturn;
+	}
 
 	public static void summary(Portfolio[] report, User[] p, Asset[] a) {
 		for (Portfolio s : report) {
-			double totalValue = Value(s.getAssetList(), a);
-			double aggregateRisk = Risk(s.getAssetList(), a, totalValue);
+			double totalValue = getTotalValue(s.getAssetList(), a);
+			double aggregateRisk = getAggregateRisk(s.getAssetList(), a, totalValue);
+			
 			System.out.println();
-
 		}
 	}
 }
