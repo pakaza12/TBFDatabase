61ffd49e3ad26e7a23f3e8d034902bfc371803fd
Jayden <jayden.carlon13@gmail.com>
2020-02-26 20:14:27 -0600

    Added risk and value

--- src/com/tbf/portfolioReport.java
+++ src/com/tbf/portfolioReport.java
@@ -28,6 +28,29 @@ public class portfolioReport {
 	}
 	
 	
+	public static double risk(HashMap<String, Double> a, Asset[] i, double value) {
+		double risk = 0;
+		//ω=b+e(−125,500/v)= 0.32 +e(−125,500/1212500.00)= 1.22167
+		for(HashMap.Entry<String, Double> c : a.entrySet()) {
+			for(int j = 0; j < i.length; j++) {
+				if(i[j] instanceof PrivateInvestment) {
+					double constant = (-125500)/((PrivateInvestment)c).getTotalValue();
+					double omega = ((PrivateInvestment)c).getBaseOmegaMeasure() + Math.exp(constant);
+					risk += omega*(c.getValue()/value);
+				}else if(i[j] instanceof Stocks) {
+					double beta = ((Stocks)c).getBaseRateOfReturn() * ((Stocks)c).getSharePrice() * c.getValue() + 4 * ((Stocks)c).getQuarterlyDividend() *c.getValue();
+					risk += beta*(c.getValue()/value);
+				}
+		}
+		}
+
+				
+		
+		
+		return risk;
+	}
+	
+	
 	
 	
 	public static void summary (Portfolio[] report, User[] p, Asset[] a) {
