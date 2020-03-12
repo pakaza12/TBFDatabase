d5e7d5141ffb33147012e9e105d49a9e177fffb9
Parker Zach <pkzach123@gmail.com>
2020-03-12 14:15:56 -0500

    Update portfolioQueries.sql

--- data/portfolioQueries.sql
+++ data/portfolioQueries.sql
@@ -1,1 +1,8 @@
 -- Query to retrieve major fields
+
+-- Query to return the email(s) for a person
+select e.email from Email e left join Person p on p.personId = e.personId where p.personCode = '1AB'; -- Replace 11AB with the desired userCode
+-- Query to add an email to a person
+insert into Email(personId, email) values ((select p.personId from Person p where p.personCode = '1AB'), 'hsalazar@msn.com'); -- Change the user code that you want to add to then add the email address
+-- A query to change the email address of a given email record
+update Email set email = 'hsalazar0@gmail.com' where email = 'hsalazar0@apache.org';
